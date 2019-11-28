928bec1208200924bc54964401d61303e2253049
Vambow <vambow@icloud.com>
2019-11-28 15:14:14 -0500

    Make changes to multipara.cc

--- function/multipara.cc
+++ function/multipara.cc
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int addNumbers(int x, int y)
-{ int result = x+y;
+int addNumbers(int x, int y, int z)
+{ int result = x+y+z;
   return result;
 }
 
 int main()
 {
-	cout << addNumbers(50,25);
+	cout << addNumbers(50,25,70)<<'\n';
 }
